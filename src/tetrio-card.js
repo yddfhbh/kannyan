89f7f11d270ada8091bd8d59536733eca9a4cb5d
yddfhbh <yddfhbh@gmail.com>
2026-06-12 14:14:25 +0900

    수치딸깍쇼

--- src/tetrio-card.js
+++ src/tetrio-card.js
@@ -614,7 +614,7 @@ async function readLocalImageDataUri(path, mimeType) {
 }
 
 async function renderTetrioCardSvg(user, summaries, assets) {
-  const svgWidth = 790;
+  const svgWidth = 780;
   const layoutWidth = svgWidth;
   const cardX = 14;
   const cardWidth = layoutWidth - cardX * 2;
